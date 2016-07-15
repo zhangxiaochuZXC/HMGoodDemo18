b990616390d202fa58bf9426d7e0ead082c6fafc
ITManager <zhangjiesharp@163.com>
2016-07-16 07:28:10 +0800

    实现了第一个功能0.0.1

---
+++ HMGoodDemo18/HMGoodDemo18/Class/TestObject.h
@@ -0,0 +1,15 @@
+//
+//  TestObject.h
+//  HMGoodDemo18
+//
+//  Created by zhangjie on 16/7/16.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+- (void)start;
+
+@end
---
+++ HMGoodDemo18/HMGoodDemo18/Class/TestObject.m
@@ -0,0 +1,18 @@
+//
+//  TestObject.m
+//  HMGoodDemo18
+//
+//  Created by zhangjie on 16/7/16.
+//  Copyright © 2016年 zhangjie. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+- (void)start
+{
+    NSLog(@"%s",__func__);
+}
+
+@end
